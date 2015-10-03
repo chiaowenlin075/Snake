6f1cec72178920b8f2f09c24b89e2843ff99777c
Leah Lin <chiaowenlin075@gmail.com>
2015-10-02 22:34:36 -0400

    Change snake speed

--- js/snake-view.js
+++ js/snake-view.js
@@ -102,10 +102,10 @@
                                    .data("speed", 0.07);
     var $normalOption = $("<button>").text("Regular")
                                      .addClass("speed-normal speed-option")
-                                     .data("speed", 0.13);
+                                     .data("speed", 0.12);
     var $slowOption = $("<button>").text("Beginner")
                                    .addClass("speed-slow speed-option")
-                                   .data("speed", 0.4);
+                                   .data("speed", 0.18);
     var $buttonDiv = $("<div class='speed-choices group'>");
     $buttonDiv.append($fastOption, $normalOption, $slowOption);
     this.$snakeContent.prepend($buttonDiv);
